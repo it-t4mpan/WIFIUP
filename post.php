9a9bc50027faa11809b97ddbb46e488808021144
ampun suhu <168879273+it-t4mpan@users.noreply.github.com>
2024-06-24 07:41:24 +0700

    Update post.php
    fix xss, csrf

--- post.php
+++ post.php
@@ -1,18 +1,19 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $nama = htmlspecialchars($_POST['nama']);
-    $nrk = htmlspecialchars($_POST['nrk']);
-    $tempat_tugas = htmlspecialchars($_POST['tempat_tugas']);
+    $nama = filter_var($_POST['nama'], FILTER_SANITIZE_STRING);
+    $nrk = filter_var($_POST['nrk'], FILTER_SANITIZE_STRING);
+    $tempat_tugas = filter_var($_POST['tempat_tugas'], FILTER_SANITIZE_STRING);
 
     // Handle file upload
     if (isset($_FILES['foto']) && $_FILES['foto']['error'] == UPLOAD_ERR_OK) {
         $allowedMimeTypes = ['image/jpeg', 'image/jpg'];
         $fileMimeType = mime_content_type($_FILES['foto']['tmp_name']);
-        if (in_array($fileMimeType, $allowedMimeTypes)) {
+        $fileExtension = pathinfo($_FILES['foto']['name'], PATHINFO_EXTENSION);
+        
+        if (in_array($fileMimeType, $allowedMimeTypes) && in_array($fileExtension, ['jpg', 'jpeg'])) {
             $fotoPath = 'uploads/' . uniqid() . '-' . basename($_FILES['foto']['name']);
             if (move_uploaded_file($_FILES['foto']['tmp_name'], $fotoPath)) {
-                // Assuming your domain is example.com and uploads folder is publicly accessible
-                $fotoUrl = 'https://example.com/wifiup/' . $fotoPath;
+                $fotoUrl = 'https://localhost/WIFIUP/' . $fotoPath;
             } else {
                 die('Upload foto gagal.');
             }
@@ -24,7 +25,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     }
 
     // Generate timestamp
-    date_default_timezone_set('Asia/Jakarta'); // Sesuaikan dengan timezone Anda
+    date_default_timezone_set('Asia/Jakarta');
     $timestamp = date('d-m-Y H:i:s');
 
     // Prepare the data
@@ -57,10 +58,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $message .= "Foto: $fotoUrl\n";
     $message .= "Timestamp: $timestamp\n";
 
-    // URL encode the message to be sent to WhatsApp
-    $whatsappUrl = "https://wa.me/62800000000?text=" . urlencode($message);
+    $whatsappUrl = "https://wa.me/6281210019202?text=" . urlencode($message);
 
-    // Return JSON response with WhatsApp URL
     echo json_encode(['success' => true, 'whatsapp_url' => $whatsappUrl]);
     exit();
 } else {
